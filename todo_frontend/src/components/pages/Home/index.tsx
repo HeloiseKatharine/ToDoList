e720608716fe516782490a66bae0da6d2ece9e7c
HeloiseKatharine <katharineheloise@gmail.com>
2024-08-03 17:14:00 -0300

    feat: cria página "Home"

---
+++ todo_frontend/src/components/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import TaskList from "../../layout/TaskList";
+import Register from "../Register";
+import Login from "../Login";
+
+function Home() {
+  return (
+    <div className="Home">
+      {/* <Register /> */}
+      {/* <Login/> */}
+      <TaskList />
+    </div>
+  );
+}
+
+export default Home;
